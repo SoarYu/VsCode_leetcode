acca75f6ce5e6e838b87251f1c5571ff0caff3a7
SoarYu <349815610@qq.com>
2022-10-27 21:27:16 +0800

    Ywg (#2)
    * add dirs
    
    * add len(grid)
    
    * add 934

--- 934.最短的桥_answer.go
+++ 934.最短的桥_answer.go
@@ -1,55 +1,45 @@
-/*
- * @lc app=leetcode.cn id=934 lang=golang
- *
- * [934] 最短的桥
- */
-
-// @lc code=start
-func shortestBridge(grid [][]int) (step int) {
-    type pair struct{ x, y int }
-    dirs := []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
-    n := len(grid)
-    for i, row := range grid {
-        for j, v := range row {
-            if v != 1 {
-                continue
-            }
-            q := []pair{}
-            var dfs func(int, int)
-            dfs = func(i, j int) {
-                grid[i][j] = -1
-                q = append(q, pair{i, j})
-                for _, d := range dirs {
-                    x, y := i+d.x, j+d.y
-                    if 0 <= x && x < n && 0 <= y && y < n && grid[x][y] == 1 {
-                        dfs(x, y)
-                    }
-                }
-            }
-            dfs(i, j)
-
-            for {
-                tmp := q
-                q = nil
-                for _, p := range tmp {
-                    for _, d := range dirs {
-                        x, y := p.x+d.x, p.y+d.y
-                        if 0 <= x && x < n && 0 <= y && y < n {
-                            if grid[x][y] == 1 {
-                                return
-                            }
-                            if grid[x][y] == 0 {
-                                grid[x][y] = -1
-                                q = append(q, pair{x, y})
-                            }
-                        }
-                    }
-                }
-                step++
-            }
-        }
-    }
-    return
+func shortestBridge(grid [][]int) (ans int) {
+	n := len(grid)
+	dirs := []int{-1, 0, 1, 0, -1}
+	type pair struct{ i, j int }
+	q := []pair{}
+	var dfs func(int, int)
+	dfs = func(i, j int) {
+		grid[i][j] = 2
+		q = append(q, pair{i, j})
+		for k := 0; k < 4; k++ {
+			x, y := i+dirs[k], j+dirs[k+1]
+			if x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 1 {
+				dfs(x, y)
+			}
+		}
+	}
+	for i, x := 0, 1; i < n && x == 1; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 1 {
+				dfs(i, j)
+				x = 0
+				break
+			}
+		}
+	}
+	for {
+		for i := len(q); i > 0; i-- {
+			p := q[0]
+			q = q[1:]
+			for k := 0; k < 4; k++ {
+				x, y := p.i+dirs[k], p.j+dirs[k+1]
+				if x >= 0 && x < n && y >= 0 && y < n {
+					if grid[x][y] == 1 {
+						return
+					}
+					if grid[x][y] == 0 {
+						grid[x][y] = 2
+						q = append(q, pair{x, y})
+					}
+				}
+			}
+		}
+		ans++
+	}
 }
-// @lc code=end
-
