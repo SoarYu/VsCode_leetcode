b3197f1f2c521ee349e17bc02db667eaf7b1b421
SoarYu <349815610@qq.com>
2022-11-04 10:04:40 +0800

    1104_daily

--- 1668.最大重复子字符串.go
+++ 1668.最大重复子字符串.go
@@ -8,14 +8,17 @@
 func maxRepeating(sequence string, word string) int {
 	m, n := len(sequence), len(word) // 5 - 2 = 3  
 	res := 0
-	for i:=0; i<=m-n; i++ {
+	for i:=0; i<=m-n; i++{
 		tmp := 0
 		for i<=m-n && sequence[i:i+n] == word {
 			tmp++
 			i += n
 		}
-		if tmp > res {
-			res = tmp
+		if tmp > 0 {
+			if tmp > res {
+				res = tmp
+			}
+			i -= n
 		}
 	}
 	return res
@@ -23,6 +26,7 @@ func maxRepeating(sequence string, word string) int {
 // @lc code=end
 
 /*
-"aaabaaaabaaabaaaabaaaabaaaabaaaaba"
+交汇点只有一个
+"aaabaaaabaaaaba aabaaaabaaaabaaaabaaaaba"
 "aaaba"
 */
---
+++ 754.到达终点数字.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=754 lang=golang
+ *
+ * [754] 到达终点数字
+ */
+
+// @lc code=start
+func reachNumber(target int) int {
+
+}
+// @lc code=end
+
