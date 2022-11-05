b2491a5d52efb4772d0564eb09c46da9bae9a4bc
SoarYu <349815610@qq.com>
2022-11-05 14:31:05 +0800

    1105_daily

---
+++ 1106.解析布尔表达式.go
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=1106 lang=golang
+ *
+ * [1106] 解析布尔表达式
+ */
+
+// @lc code=start
+func parseBoolExpr(expression string) bool {
+	opsStack := []rune{}
+	t, f := 0, 0
+	for _, ch := range expression {
+		if ch == 't' {
+			t++
+		} else if ch == 'f' {
+			f++
+		} else if ch=='!' || ch=='&' || ch=='|' || ch=='(' {
+			opsStack = append(opsStack, ch)
+			f, t = 0, 0
+		} else if ch == ')'{
+			ops := opsStack[len(opsStack)-1]
+			opsStack = opsStack[:len(opsStack)-1]
+			for ops != '(' {
+				if ops == 't' {
+					t++
+				} else if ops == 'f' {
+					f++
+				}
+				ops = opsStack[len(opsStack)-1]
+				opsStack = opsStack[:len(opsStack)-1]
+			}
+			ops = opsStack[len(opsStack)-1]
+			opsStack = opsStack[:len(opsStack)-1]
+			if ops == '!' {
+				if t>0 {
+					opsStack = append(opsStack, 'f')
+				} else if f > 0 {
+					opsStack = append(opsStack, 't')
+				}
+			} else if ops == '&' {
+				if f > 0 {
+					opsStack = append(opsStack, 'f')
+				} else {
+					opsStack = append(opsStack, 't')
+				}
+			} else if ops == '|' {
+				if t > 0 {
+					opsStack = append(opsStack, 't')
+				} else {
+					opsStack = append(opsStack, 'f')
+				}
+			}
+			f, t = 0, 0
+		}
+	}
+	return opsStack[0] == 't'
+}
+// @lc code=end
+
