8b3fd2253c1cd37de02c8ce2869fb997e33e1165
SoarYu <349815610@qq.com>
2022-11-19 10:07:46 +0800

    1119_daily

---
+++ 1732.找到最高海拔.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1732 lang=golang
+ *
+ * [1732] 找到最高海拔
+ */
+package main
+// @lc code=start
+func largestAltitude(gain []int) int {
+	max, cur := 0, 0
+	for _, h := gain {
+		if cur += h; cur > max {
+			max = cur
+		}
+	}
+	return max
+}
+// @lc code=end
+
